b07f47112328877e8a64dc4b7f3914c25b5d5121
ApelMahmud65 <apelmahmud.kpi@gmail.com>
2020-08-16 09:38:22 +0800

    database connected success

--- index.js
+++ index.js
@@ -13,30 +13,45 @@ const port = process.env.DB_PORT || 4200;
 let client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //GET
-app.get('/persons', (req, res) => {
+app.get('/foods', (req, res) => {
   client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
   client.connect(err => {
-    const collection = client.db("onlineUser").collection("user");
-    collection.find().toArray((err, document) => {
+    const collection = client.db("FoodStore").collection("foods");
+    collection.find().toArray((err, documents) => {
         if(err){
           console.log('This is error', err);
         }
         else{
-          res.send(document);
+          res.send(documents);
         }
         client.close();
     })
   });
 })
 
+//Daynamic foods
+app.get('/food/:key', async(req, res) => {
+  const key = req.params.key;
+
+  client = await MongoClient.connect(uri, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+  });
+  const db = client.db('FoodStore');
+  const items = await db.collection('foods').find({key}).toArray();
+  res.send(items[0]);
+  client.close();
+})
+
+
 //POST
-app.post('/addPerson', (req, res) => {
+app.post('/addFood', (req, res) => {
   client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
   const person = req.body;
     
     client.connect(err => {
-      const collection = client.db("onlineUser").collection("user");
-      collection.insertOne(person, (err, result) => {
+      const collection = client.db("FoodStore").collection("foods");
+      collection.insert(person, (err, result) => {
           if(err){
             console.log('This is error', err);
             res.status(500).send({message:err});
